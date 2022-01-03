ada144c0cf8f8c65877cffc5634236a2e6b49087
Emmanuel Lopez <jesusemm010@hotmail.com>
2022-01-02 23:08:16 -0600

    getting started with the first tests

---
+++ src/tests/HookApp.test.js
@@ -0,0 +1,12 @@
+import {HookApp} from "../HookApp";
+import {shallow} from "enzyme";
+import "../setupTest";
+
+describe("Testing <HookApp/>", () => {
+
+    test("Should't have any errors", ()=> {
+        const wrapper = shallow( <HookApp /> );
+        
+        expect(wrapper).toMatchSnapshot();
+    })
+})
